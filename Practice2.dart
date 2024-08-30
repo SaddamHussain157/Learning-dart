dd5ab13f2be2b90daa3dfbd70abccc011174ef33
Saddam <saddamhussainsumalani109gmail.com>
2024-08-30 19:02:27 +0500

    more questio sloved

--- Practice2.dart
+++ Practice2.dart
@@ -26,13 +26,62 @@ CheckWeatherConstorvowel() {
       vowel == 'u' ||
       vowel == 'U') {
     print('Its vowel: $vowel');
-  }
-   else {
+  } else {
     print('Not a vowel: $vowel');
   }
 }
 
+// Q3. Write a dart program to check whether a number is positive, negative, or zero.
+checkingNumberWeatherZeroPosNeg() {
+  print('Enter a Number: ');
+  int number = int.parse(stdin.readLineSync()!);
+  if (number == 0) {
+    print('The Number is Zero: ');
+  } else if (number > 0) {
+    print('The number is Positve: ');
+  } else {
+    print('The Number is Negative: ');
+  }
+}
+
+// Q4. Write a dart program to print your name 100 times.
+PrintName100Times() {
+  // As your wish which loop you want to use;
+  //1 For Loop
+  for (var i = 1; i <= 100; i++) {
+    print('My name is: <Saddam Hussain> $i');
+  }
+  // // 2 While loop
+  var name = 1;
+  while (name <= 100) {
+    print('My name is Saddam Hussain: $name');
+    name++;
+  }
+  var name2 = 1;
+  do {
+    print('I am Myself Saddam hussain: $name2');
+    name2++;
+  } while (name2 <= 100);
+}
+
+// Q5. Write a dart program to calculate the sum of natural numbers.
+sumOfNutralNO() {
+  var supposeNumber = 0;
+  print('Please enter the intitial Value: ');
+  int intitialValue = int.parse(stdin.readLineSync()!);
+  print('Enter the Final value Which must b Greater then initial value: ');
+  int FinalValue = int.parse(stdin.readLineSync()!);
+  while (intitialValue <= FinalValue) {
+    supposeNumber += intitialValue;
+    intitialValue++;
+  }
+  print(supposeNumber);
+}
+
 void main(List<String> args) {
-  CheckWeatherConstorvowel();
+  sumOfNutralNO();
+  // PrintName100Times();
+  // checkingNumberWeatherZeroPosNeg();
+  // CheckWeatherConstorvowel();
   // NumberCheckWeatherNoisOddEven();
 }
