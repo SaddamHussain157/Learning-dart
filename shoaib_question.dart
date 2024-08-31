a510e7cb2baf636baa73d4a73a19d1c475277b23
Muhammad Shoaib <shoaibsn54321@gmail.com>
2024-08-31 23:18:46 +0500

    Question solve

---
+++ shoaib_question.dart
@@ -0,0 +1,20 @@
+main() {
+  var obj = QuestionPartOne();
+  var test = obj.frequency("the is my first application how to solve it");
+  print(test == 2);
+  test = obj.frequency("aaaaaaaa");
+  print(test == 8);
+  test = obj.frequency("bbbba");
+  print(test == 1);
+  test = obj.frequency("bbbb");
+  print(test == 0);
+  
+}
+
+class QuestionPartOne {
+  int frequency(String n) {
+    // complete the method
+    // return how times a will come in the string 
+    return 0;
+  }
+}
